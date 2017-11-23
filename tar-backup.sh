7c831221abed224d6e2778f15a0567be568a677e
Alex Wilczewski <alex@wilczewski.org>
2017-11-23 11:38:14 -0500

    Added backup by sh.

---
+++ tar-backup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+date_as_string=`date +%Y-%m-%d`
+
+shopt -s dotglob
+cd /
+tar -cvpzf /tmp/backup-$date_as_string.tar.gz \
+    --exclude=/proc \
+    --exclude=/lost+found \
+    --exclude=/tmp \
+    --exclude=/mnt \
+    --exclude=/dev \
+    --exclude=/sys \
+    --exclude=/run \
+    --exclude=/media \
+    --exclude=/var/log \
+    --exclude=/var/cache/apt/archives \
+    --exclude=/usr/src/linux-headers* \
+    --exclude=/home \
+    --exclude=/home/*/.gvfs \
+    --exclude=/home/*/.cache \
+    --exclude=/home/*/.local/share/Trash *
+shopt -u dotglob
