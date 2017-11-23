0cb85b81acb4e9ad740d7bd9665f9434c7a00d35
Alex Wilczewski <alex@wilczewski.org>
2017-11-23 12:21:12 -0500

    Removed absolute pathing.

--- tar-backup.sh
+++ tar-backup.sh
@@ -5,19 +5,18 @@ date_as_string=`date +%Y-%m-%d`
 shopt -s dotglob
 cd /
 tar -cvpzf /tmp/backup-$date_as_string.tar.gz \
-    --exclude=/proc \
-    --exclude=/lost+found \
-    --exclude=/tmp \
-    --exclude=/mnt \
-    --exclude=/dev \
-    --exclude=/sys \
-    --exclude=/run \
-    --exclude=/media \
-    --exclude=/var/log \
-    --exclude=/var/cache/apt/archives \
-    --exclude=/usr/src/linux-headers* \
-    --exclude=/home \
-    --exclude=/home/*/.gvfs \
-    --exclude=/home/*/.cache \
-    --exclude=/home/*/.local/share/Trash *
+    --exclude=proc \
+    --exclude=lost+found \
+    --exclude=tmp \
+    --exclude=mnt \
+    --exclude=dev \
+    --exclude=sys \
+    --exclude=run \
+    --exclude=media \
+    --exclude=var/log \
+    --exclude=var/cache/apt/archives \
+    --exclude=usr/src/linux-headers* \
+    --exclude=home/*/.gvfs \
+    --exclude=home/*/.cache \
+    --exclude=home/*/.local/share/Trash *
 shopt -u dotglob
